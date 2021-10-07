15e4a895921d4fddb06470097e00d79f34e40a1e
Jon Are Rakvåg <git@jonare.no>
2021-10-07 12:23:08 +0200

    Direct fingerprinting of interesting NS servers in dns-trace.sh

--- dns-trace.sh
+++ dns-trace.sh
@@ -1,9 +1,37 @@
 #!/bin/bash
 
-takeoverfile="dns-trace-$(date +%d%m%y-%H%M).txt"
+tracefile="dns-trace-$(date +%d%m%y-%H%M).txt"
 
 while read -r subdomain; do
-  dig $subdomain +trace >> "$takeoverfile"
+  dig $subdomain +trace >> "$tracefile"
 done < $1
 
-echo "$takeoverfile"
+echo "Wrote $tracefile"
+
+declare -a fingerprints=(
+                "000domains.com"
+                "azure-dns"
+		"bizland.com"
+		"click2site.com"
+		"digitalocean.com"
+		"dnsmadeeasy.com"
+		"dnsimple.com"
+		"ns1.domain.com"
+		"ns2.domain.com"
+		"dotster.com"
+		"nameresolve.com"
+		"easydns"
+		"he.net"
+		"linode.com"
+		"mydomain.com"
+		".name.com"
+		"nsone.net"
+		"domaindiscover.com"
+		"reg.ru"
+		"yns1.yahoo.com"
+		"yns2.yahoo.com"
+                 )
+
+for i in "${fingerprints[@]}"; do
+   grep "NS" "$tracefile" | grep -h "$i" && echo -e "\e[1;32mInteresting NS if lookup failed:\e[0m Pattern: $i. See https://github.com/indianajson/can-i-take-over-dns"
+done
